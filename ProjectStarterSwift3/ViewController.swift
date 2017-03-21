67fae068bb6419f3c07ce2c421ce1f68e0e30881
Meilissa Lee <meilissa@node8.net>
2017-03-21 13:57:00 +0700

    Initial Commit

---
+++ ProjectStarterSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectStarterSwift3
+//
+//  Created by Meilissa Lee on 3/21/17.
+//  Copyright © 2017 Meilissa Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
